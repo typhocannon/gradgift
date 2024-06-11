d432e41b9a6b67ac1ca07c79337fb0a6a30035d8
Casey Tran <68321284+typhocannon@users.noreply.github.com>
2024-06-11 16:27:02 -0700

    added background image lol

--- goodbyedylan/src/App.tsx
+++ goodbyedylan/src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
 import './App.css'
-
+import { BackgroundGradientAnimation } from './components/background-gradient-animation'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+      <div className="w-screen v-screen">
+      <BackgroundGradientAnimation>
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+      </BackgroundGradientAnimation>
       </div>
-    </>
   )
 }
 
