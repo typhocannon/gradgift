c649e95bdc04ad8dabaf9c112497774b5ad11469
Casey Tran <68321284+typhocannon@users.noreply.github.com>
2024-06-15 01:55:52 -0700

    adding more messages

--- goodbyedylan/src/assets/messages.ts
+++ goodbyedylan/src/assets/messages.ts
@@ -37,45 +37,110 @@ export const messages = [
         image: ["/jose_0.jpg"],
         className: "lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-4",
     },
+    {
+      name: "Thu",
+      description:
+      "Hello Dylan, Congratulations on finishing your undergraduate degree! Firstly, I would like to say I am so glad we met in the chem house; otherwise, your girl would have starved from having zero cooking equipment. You are another positive and cheerful presence in my life, which I really appreciate. From inviting me rock climbing, helping me move my shit, letting me raid your cooking supply, sharing food, belaying so that Jasper doesn’t kill me, the San Diego trip, the birthday dinner, and the list goes on, you have been more than just helpful. You have been like a fairy godmother to me (fairy capybara ???). I haven’t met anyone more genuinely sweet, caring, and thoughtful. Thank you for letting me leach from your resources and also for being my friend. I am pretty sure you will keep slayyyying at life like you already have. Mong kiệp này được gặp anh dài dài (I hope to keep seeing you in this life time)",
+      image: ["https://firebasestorage.googleapis.com/v0/b/venushacks2024-777a4.appspot.com/o/737E2FB4-C7EA-4776-9C83-04F9A3E48DCC.jpg?alt=media&token=5b763654-c109-4c85-ad7c-91867ee2e15e", "https://firebasestorage.googleapis.com/v0/b/venushacks2024-777a4.appspot.com/o/910B5A63-73CB-4E52-9E32-91018A47F574.jpg?alt=media&token=ad5a6d4b-1830-471e-a31a-840704c46786"],
+      className: "lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-4"
+    },
+    {
+      name: "Ileen",
+      description:
+      "hiiii dylan congrats on gradding! i’m so grateful to have met you last year through icssc. i’ve really enjoyed all the time and fun i've spent with you, including learning how to drive, visiting you in sac, climbing with you, taking grad pics together, and so much more. you’re a really great friend and i’m so lucky to have someone like you by my side. congrats again, and good luck with amazon (one medical)!! visit me in the bay :DD",
+      image: ["https://firebasestorage.googleapis.com/v0/b/venushacks2024-777a4.appspot.com/o/Screenshot%202024-06-12%20at%2010.10.53%E2%80%AFPM.png?alt=media&token=7043ecd1-c1e9-4a12-baf3-3d651d8dbe11"],
+      className: "lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-4"
+    },
+    {
+      name: "Justin",
+      description:
+      "Hi Dylan, I know I haven't known you for long, but for the time I have, you've always been very friendly and welcoming and it has really made my experience here at UCI better when I get to hang out with you and the others. Congrats on graduating!! :DD p.s. I support the Justin x Dylan ship :)",
+      image: [],
+      className: "lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-4"
+    },
+    {
+      name: "Katherine",
+      description:
+      "Congrats on graduating! It was fun working with you for senior design. You're the best video editor I've ever met haha. Good luck with your future endeavors!",
+      image: [],
+      className: "lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-4"
+    },
+    {
+      name: "Ariel",
+      description:
+      `Hey Dylan, Congratulations on graduating with the class of 2024! You've made it!
+
+      Over the years, I've come to admire your courage and tenacity in chasing your dreams. It probably wasn't an easy decision to leave UCSB to pursue a CS degree – not just because you disliked chemical engineering, right? Those nights cramming for thermodynamics were fun, weren't they? ≖_≖ But you did more than just get by; you thrived and seemed to enjoy every bit of it.
+
+      I won't lie, our friendship has significantly influenced my life. Your cooking Instagram inspired me to cook edible food. No more salad hot pot for me now. There's so much more I want to say but lack the words to express.
+
+      May your future be exciting and the climbing gyms ever challenging.
+
+      Here's a little surprise from Stephanie, Abhiram, Alan, and me: m/dylando
+      It's the third part of the puzzle. ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧
+
+
+      - Ariel Kim
+      `,
+      image: [],
+      className: "lg:col-start-4 lg:col-end-5 lg:row-start-2 lg:row-end-4"
+    },
+    {
+      name: "Cristian",
+      description:
+      `Hey Dylan! Congratulations on graduating! You're an amazing friend I'm so glad I got to meet you. From the meals last school year to shared classes this school year, as our friendship grew I felt more and more grateful to have met you. Your mind is such an intelligent one, you're an awe-inspiring person. As we go our separate ways into post-grad life I will make sure to cherish our shared experiences, conversations, ideas, and memories. You have a way of making people feel appreciated, seen, and cared for. I wish you the best in your future endeavors and will definitely make sure to keep in contact! Congrats on graduating Dylan!
+      From EECS 195 Buddy,
+      Cristian Chun Yoxon`,
+      image: [],
+      className: "lg:col-start-4 lg:col-end-5 lg:row-start-2 lg:row-end-4"
+    },
+    {
+      name: "Nathan",
+      description:
+      `Hello Dylan,
+
+      Congratulations on graduating! It was nice meeting you (I forgot how we met, I'm guessing through ICSSC), and I'm glad we met because you're a really cool, chill guy! It's always fun bouldering with you, and I hope we can boulder sometime again in the future! Keep on doing great things, and I wish you all the best! 
+
+      Best,
+      Nathan Nguyen
+      `,
+      image: [],
+      className: "lg:col-start-4 lg:col-end-5 lg:row-start-2 lg:row-end-4"
+      
+    },
+    {
+      name: "Alicia",
+      description:
+      `
+      Heyy Dylan, it's me, Alicia. First, CONGRATULATION!!. I still can't believe we are graduating tho. Having you as a friend for my entire school years after I transferred was really good. I never said yes when you invited me to your rock climbing party but at least I suffered many classes with you. 13 hours at brandy when we took ICS 53 was crazy even end up we started playing games tho as usual. It was a fun school year with you. I hope you also made a lot of great memories. Keep in touch even after we graduate!  
+      `,
+      image: ["https://firebasestorage.googleapis.com/v0/b/venushacks2024-777a4.appspot.com/o/Dylan%20suffering%20by%20ICS53%20Exam.jpg?alt=media&token=b99fbf7c-2549-4d9b-ac1e-dfb878caab1c"],
+      className: "lg:col-start-4 lg:col-end-5 lg:row-start-2 lg:row-end-4"
+    },
+    {
+      name: "Priyanka",
+      description:
+      `Some guy I knew from high school once quoted Stephen Hawking, saying the following:
+      “The universe doesn’t allow perfection. Well I guess the universe forgot about me.”
+
+      So actually it’s a crazy coincidence…but that guy was you (woah so crazy…).
+
+      And that was, crazy enough, five years ago, when we graduated from Vista, before COVID, before you transferred to Irvine, before you knew your true calling of being a beep boop legend. I mean, you already were a legend by the time we had graduated; I don’t think anyone was making Honors Chem study guides the way you were before our time, and I know that no one is now. And I am triple positive that no one is cracked enough to be teaching calculus to kids while currently enrolled in the class (if you were to look up academic weapon it would actually just be a bunch of pictures of Dylan Vu). 
+
+      Despite all the changes throughout the years, one thing remained consistent: your genuinity. Perhaps this quality is what you were referring to when you dropped that bar in our yearbook. Your ability to influence and inspire honestly needs to be studied because it’s crazy. Whether it be helping me through my awful time in Matlab, your mentorship in the clubs you’re involved in, or even just being there for a friend -  it is clear that you care about people and help everyone without asking for anything in return. 
+
+      You’ve continued to inspire me throughout the years with your outlook on challenges. Maybe it’s the problem solver in you, but you don’t let things stunt your growth, and this approach to life is something that I myself have learned to appreciate. Obviously, when I’m stuck, I can just bother you LOL. But this whole rolling with the punches thing? Brilliant.
+
+      I’m really glad 8th grade us decided to become “cronies” on a random weekday. Who knew that would turn into over a decade of friendship. We have come a long way together, and I can’t wait to see what the future brings for us. 
+
+      Congratulations on graduating DVu!! :)
+
+      Priyanka
+
+      (P.S: When you're super rich and famous, pls buy me a car.)
+      `,
+      image:["https://firebasestorage.googleapis.com/v0/b/venushacks2024-777a4.appspot.com/o/IMG_1210.JPG?alt=media&token=38ba602d-40f2-4aea-9697-a53c92a761ad", "https://firebasestorage.googleapis.com/v0/b/venushacks2024-777a4.appspot.com/o/IMG_3508.jpg?alt=media&token=8c92ed6d-ba06-4288-8e2a-6c7fa7466da7"],
+      className: "lg:col-start-2 lg:col-end-5 lg:row-start-2 lg:row-end-4"
+    }
   ];
 
-export const messages2 = [
-    {
-      name: "Anthony",
-      description: "Happy Graduation Dylan! I hope you have a fun day and do things you’ll remember and cherish. Thanks for being such a joy to be around. It might not seem like it but hanging out and doing stuff like hugging a watermelon really made my week sometimes and I appreciate it. You’re the best!",
-      image: [],
-      className: "lg:row-start-1 lg:row-end-1 lg:col-start-2 lg:col-end-2",
-    },
-    {
-      name: "Sandra",
-      description: "Heyyy Dylan!! Congrats on your graduation! You are one of the most impressive and selfless people I know and I will miss you dearly next year. Thanks for all the fun laughs and memories especially the sleepy ones haha. I hope that you buy yourself a nice bike too! We can go on a ride someday! But rest assured we will always have Kai to help us carry our broken items lol.\nP.S. Don't forget to stay away from those blue markers, or else you will end up blue-handed...\nBest,\nSandra :)",
-      image: ["/sandra_0", "/sandra_1", "/sandra_2"],
-      className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
-    },
-    {
-      name: "Rebecca",
-      description: "Hi Dylan, this is Rebecca Ahn from ICSSC! First of all, congrats on graduating!! Even though your time here at ICSSC was only for a couple of years, you made such amazing contributions, which I truly appreciate. Having been your events co-chair last year and a fellow events member this past year, I applaud your dedication to organizing cool events like the Discord Bot workshop and the IoT workshop. It was fun working with you, and I know all of us events members will miss you. I pray that your journey after UCI will go well :D",
-      image: [],
-      className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
-    },
-    {
-      name: "Jeffery",
-      description: "Dylan, congrats on graduating! it’s been so great getting to know you and working with you on events committee these past two years. I hope you have a great time working and wish you the best of luck in all your future endeavors!",
-      image: [],
-      className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
-    },
-    {
-      name: "Your Justin",
-      description:
-        "Heyo Dylan, Congrats on graduating! With your hard work and dedication, you finally made it! Who would have known that the Chem house would bring us together. For someone with absolutely no cooking skills or car, thanks for being my personal chef and Uber though the directions could use some work. Also better fix that car as I’ll still need rides next year lol. I have learned so much from you, how to play League, Apex, scramble an egg, oh and in school as well as I am pretty sure you are a better electrical engineer than myself.  I am glad to say that I haven’t picked up on your study habits or sleep schedule either. But for real, you are truly one of the most talented and selfless people I have ever met. I will always cherish the memories that we have whether it be the spontaneous boba runs, trips, senior design project, hackathons (thanks for always repping hardware), and probably so much more I’m forgetting about. I’ll miss having you as my roommate but we’ll still hang out next year so it’s all good. Enjoy working and don’t forget about all of us when you make it big time.",
-      image: ["/your_justin_0", "/your_justin_1"],
-      className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
-    },
-    {
-      name: "Jose",
-      description:
-        "HEY DYLAN it's jose I just want to say congratulations and great job on your bachelors. You deserve this moment to celebrate your accomplishments and I just want to thank you so much for all the wisdom and joy you brought into my life! I would not be the programmer and person I am today had you not decided to give back to the ICS community by being a Byte  becoming yours and casey bit was one of the best choices(if not the best choice) i made at UCI good luck(no luck needed tho, pure skillz) on your future and do great things! next rounds of boba is on me ",
-      image: ["/jose_0"],
-      className: "lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-4",
-    },
-  ]
